0dc5f8606fd24c114ee0ab82264a8ee3a3b47d13
Suraj Prakash <surajprakash094@gmail.com>
2017-10-30 02:08:09 +0530

    this is it

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ This is an app to display some cool games developed for diffrent plateforms
 with ratings and features like sort and search
 
 git repo: https://github.com/suraj094/GameArena.git
+
+
+just npm install
+npm start
--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import FaSearch from 'react-icons/lib/fa/search';
 import axios from 'axios';
 import './App.css';
 
@@ -8,7 +9,10 @@ class App extends Component {
     console.log("1")
     super(props);
     this.state = {
-      list: []
+      list: [],
+      home:true,
+      action:'',
+      search:''
     }
   }
 
@@ -30,21 +34,49 @@ class App extends Component {
      });
   
  }
+ 
+
+ searchAction = (searchvalue) => {
+  
+   this.setState(function(){
+     return{
+     action: 'search',
+     search: searchvalue
+   }
+   })
+
+   var data = this.state.list;
+   var newdata = data.filter((arr) => (arr.title === this.state.searchvalue));
+   console.log(newdata);
+   
+ }
+
+
 
   render() {
     if(this.state.list.length === 0){
       return null
     }
 
+    if(this.state.home === true){
     return (
       
       <div className="App grid-containe">
-        <Nav />
+        <Nav  localsearch = {this.searchAction}/>
       <div className='box'>
       {this.state.list.map((value,i) => (<Card key={i}   {...value} />))}
       </div>
       </div>
     );
+  }
+
+  if(this.state.action === 'search'){
+    var data = this.state.list;
+    var newdata = data.filter((arr) => {arr.title === this.state.search})
+    this.setState({home:false})
+    console.log(newdata);
+    return null
+  }
   }
 }
 
@@ -61,12 +93,40 @@ const Card = (props) => (
 
 
 class Nav extends Component{
+   constructor(props){
+     super(props);
+     this.state={
+       query:''
+     }
+   }
+
+   UpdateSearch = (event) =>{
+    event.preventDefault()
+    this.setState({query:event.target.value})
+}
+
+handleSubmit = (event) =>{
+  if(event.key === 'Enter'){
+      event.preventDefault();
+      this.props.localsearch(this.state.query);
+  }
+}
+
+handleClick = () =>{
+  this.props.localsearch(this.state.query);
+}
 
   render(){
 
     return(
-      <div>
-        <p>hi</p>
+      <div className='nav'>
+        <li className='nav-span title'>Home</li>
+        <li className='nav-span'><input type='text' className='nav-search'
+                 value={this.state.query}
+                 onChange={this.UpdateSearch}
+                 onKeyPress={this.handleSubmit} />
+                 <FaSearch className='search-icon nav-span' onClick={this.handleClick} />
+                 </li>
       </div>
     )
   }
