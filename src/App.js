c05209b98ef2d1d025833c45a9b92d4ae1ee70a9
Suraj Prakash <surajprakash094@gmail.com>
2017-10-29 23:31:40 +0530

    first stage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 class App extends Component {
 
   constructor(props){
+    console.log("1")
     super(props);
     this.state = {
       list: []
@@ -12,10 +13,11 @@ class App extends Component {
   }
 
   componentWillMount(){
+    console.log("2")
     var self = this;
     axios.get('http://starlord.hackerearth.com/gamesarena').then(function(response){
-      var data = response.data;
-      console.log(data);
+      var data = response.data.slice(1);
+      // console.log(data);
       self.setState(function(){
         return {
           list : data
@@ -30,15 +32,45 @@ class App extends Component {
  }
 
   render() {
-    // console.log(this.state.list[2])
-    var obj = this.state.list[2];
-    console.log(obj.title);
+    if(this.state.list.length === 0){
+      return null
+    }
+
     return (
-      <div className="App">
-        <p> hi </p>
+      
+      <div className="App grid-containe">
+        <Nav />
+      <div className='box'>
+      {this.state.list.map((value,i) => (<Card key={i}   {...value} />))}
+      </div>
       </div>
     );
   }
 }
+
+
+const Card = (props) => (
+  <div className='card'>
+    <p className='title'>Title: { props.title}</p>
+    <p>Platform: {props.platform}</p>
+    <p>Score: { props.score}</p>
+    <p>Editors Choice: { props.editors_choice}</p>
+    <p>Genre: { props.genre}</p>
+  </div>
+)
+
+
+class Nav extends Component{
+
+  render(){
+
+    return(
+      <div>
+        <p>hi</p>
+      </div>
+    )
+  }
+}
+
 
 export default App;
